56edb7b5ecb6438c08d786d4ed60cbb275aeffb8
maletta <mauricio_maletta@hotmail.com>
2024-05-11 02:00:08 -0300

    first commit

---
+++ Assets/Scripts2/Card.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace NBESQ_Productions
+{
+
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")] // cria um opção no menu create, 
+    //instancia esse script como um ScriptableObject
+    public class Card : ScriptableObject
+    {
+        public string cardName;
+        public List<CardType> cardTypes;
+        public int health;
+        public int damageMin;
+        public int damageMax;
+        public Sprite cardSprite;
+        public List<DamageType> damageType;
+
+    }
+
+    public enum CardType
+    {
+        Fire,
+        Earth,
+        Water,
+        Dark,
+        Light,
+        Air
+    }
+
+    public enum DamageType
+    {
+        Fire,
+        Earth,
+        Water,
+        Dark,
+        Light,
+        Air
+    }
+
+}
